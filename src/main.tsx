0f62acce18b237da3088a9251f6da656f5b5b82c
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2022-06-26 15:38:38 -0300

    ts paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { ApolloProvider } from '@apollo/client';
+import { client } from '@lib/apollo';
+import '@styles/global.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { client } from './lib/apollo';
-import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
